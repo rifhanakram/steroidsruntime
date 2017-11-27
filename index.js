9873b36d01e02581c967d577ad42cd9bcdf44231
supuncodes <supuncodes@gmail.com>
2017-11-27 17:09:54 +0530

    Code refactored

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var fs = require('fs');
+let yaml = require('js-yaml');        
 
 function Splash(port) {
     let splashString =
@@ -77,18 +78,10 @@ function FilterManager() {
     }
 }
 
-function ExecutableUnit(params, filterManager) {
+function SteroidsServiceHandler(params, filterManager) {
 
+    var settings = {};
     let currentContext = (() => {
-        /*
-        let response = {
-            statusCode: '400',
-            body: JSON.stringify({ error: 'you messed up!' }),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        };*/
-
         var contextData = { headers: { "Content-Type": "application/json" } };
         var callbackFunc;
         return {
@@ -111,39 +104,30 @@ function ExecutableUnit(params, filterManager) {
         }
     })();
 
-    var handlerName, lFunction;
+    
+    (function loadLambdaFunction(cb){
+        let dotIndex = params.lambda.lastIndexOf(".");
+        let lambdaFileName = params.lambda.substring(0, dotIndex);
+        let fileName = lambdaFileName + ".js";
+
+        let exists = fs.existsSync(fileName)
+        if (exists){
+            settings.handlerName = params.lambda.substring(dotIndex + 1);
+            settings.lFunction = require("../../" + fileName);
+        }
+    })();
 
     function getModule(cb){
-        
-        if (handlerName){
-            if (cb)
-                cb({success: true});
-        } else {
-            let dotIndex = params.lambda.lastIndexOf(".");
-            handlerName = params.lambda.substring(dotIndex + 1);
-            let lambdaFileName = params.lambda.substring(0, dotIndex);
-            let fileName = lambdaFileName + ".js";
-    
-            fs.exists(fileName,function(exists){
-                if (exists) {
-                    lFunction = require("../../" + fileName);
-                    if (cb)
-                        cb({success: true});
-                } else {
-                    if (cb)
-                        cb({ success: false, message: "Lambda function doesn't exist'" });
-                }            
-            });
-        }
+        if (settings.lFunction && settings.handlerName)
+            cb(settings.lFunction, settings.handlerName);
+         else
+            cb();
     }
-
-    getModule();
 
     function dispatchToLambda(event, context, callback) {
         
-        getModule(function (result){
-            let {success, message} = result;
-            if (success){
+        getModule(function (lFunction, handlerName){
+            if (lFunction && handlerName){
                 let callbackFunc = (error, result) => {
                     if (!result) {
                         if (error)
@@ -152,18 +136,20 @@ function ExecutableUnit(params, filterManager) {
                     callback(result);
                 };
                 context.setCallback(callbackFunc);
-                let result = lFunction[handlerName](event, context, callbackFunc);
+
+                let lambdaFunction = lFunction[handlerName];
+                lambdaFunction(event, context, callbackFunc);
             }else {
-                callback({success:false, message:message});
+                callback({success:false, message:"Lambda function doesn't exist'"});
             }
         })
         
     }
 
     return {
-        handle: (req, res, next) => {
+        handle: (req, res, next, callback) => {
             let fm = filterManager.getInstance();
-            fm.process(req, res, next, {}, (success, result) => {
+            fm.process(req, res, ()=>{}, {}, (success, result) => {
 
                 if (!success)
                     return;
@@ -176,98 +162,16 @@ function ExecutableUnit(params, filterManager) {
                     queryStringParameters: req.query ? req.query : {}
                 };
 
-                dispatchToLambda(eventObject, currentContext, (result) => {
-                    let cObj = currentContext.steroidsGetContext();
-                    let contentType = undefined;
-                    if (cObj.headers) {
-                        if (cObj.statusCode !== undefined)
-                            res.writeHead(parseInt(cObj.statusCode), cObj.headers);
-                        else
-                            res.writeHead(200, cObj.headers);
-
-                        for (let hKey in cObj.headers) {
-                            let hVal = cObj.headers[hKey] === undefined ? undefined : cObj.headers[hKey].toLowerCase();
-                            switch (hKey.toLowerCase()) {
-                                case "content-type":
-                                    contentType = hVal;
-                                    break;
-                            }
-                        }
-                    } else {
-                        if (cObj.statusCode !== undefined)
-                            res.writeHead(parseInt(cObj.statusCode));
-                        else
-                            res.writeHead(200);
-                    }
-
-                    if (!contentType)
-                        contentType = "application/json";
-
-                    let continueToNextResponse = true;
-
-                    if (contentType === "application/json") {
-                        if (typeof result.body === "string")
-                            res.write(result.body);
-                        else
-                            res.write(JSON.stringify(result.body));
-                    }
-                    else {
-                        let respType = result.body.constructor.name;
-                        switch (respType) {
-                            case "ReadStream":
-                                continueToNextResponse = false;
-
-                                result.body.on('data', (chunk) => {
-                                    res.write(chunk);
-                                });
-
-                                result.body.once('close', () => {
-                                    res.end();
-                                });
-
-                                result.body.on('error', () => {
-                                    res.end();
-                                });
-                                break;
-                            case "PassThrough":
-                                continueToNextResponse = false;
-
-                                result.body.on('readable', function () {
-                                    let data;
-                                    while (data = this.read()) {
-                                        res.write(data);
-                                    }
-                                });
-
-                                result.body.on('finish', function (err) {
-                                    res.end();
-                                });
-
-                                result.body.on('error', function () {
-                                    res.end();
-                                });
-                                break;
-                            default:
-                                res.write(result.body);
-                                break;
-                        }
-
-                    }
-
-                    if (continueToNextResponse) {
-                        res.end();
-                    }
-                    next();
+                dispatchToLambda(eventObject, currentContext, (result)=>{
+                    callback(result,currentContext);
                 });
-
             });
-
         }
     }
 }
 
 
-function MsfCore() {
+function SteroidsRuntime() {
     let restify = require('restify');
     let filterManager = new FilterManager();
     let runtimeConfig = undefined;
@@ -280,31 +184,125 @@ function MsfCore() {
         routes[method][params] = lambda;
     }
 
-    function startRoutingEngine(portNumber) {
+    function processResponse (req,res,next, result, context) {
+        let cObj = context.steroidsGetContext();
+        let contentType = undefined;
+        if (cObj.headers) {
+            if (cObj.statusCode !== undefined)
+                res.writeHead(parseInt(cObj.statusCode), cObj.headers);
+            else
+                res.writeHead(200, cObj.headers);
+
+            for (let hKey in cObj.headers) {
+                let hVal = cObj.headers[hKey] === undefined ? undefined : cObj.headers[hKey].toLowerCase();
+                switch (hKey.toLowerCase()) {
+                    case "content-type":
+                        contentType = hVal;
+                        break;
+                }
+            }
+        } else {
+            if (cObj.statusCode !== undefined)
+                res.writeHead(parseInt(cObj.statusCode));
+            else
+                res.writeHead(200);
+        }
+
+        if (!contentType)
+            contentType = "application/json";
+
+        let continueToNextResponse = true;
+
+        if (contentType === "application/json") {
+            if (typeof result.body === "string")
+                res.write(result.body);
+            else
+                res.write(JSON.stringify(result.body));
+        }
+        else {
+            let respType = result.body.constructor.name;
+            switch (respType) {
+                case "ReadStream":
+                    continueToNextResponse = false;
+
+                    result.body.on('data', (chunk) => {
+                        res.write(chunk);
+                    });
+
+                    result.body.once('close', () => {
+                        res.end();
+                        next();
+                    });
+
+                    result.body.on('error', () => {
+                        res.end();
+                        next();
+                    });
+                    break;
+                case "PassThrough":
+                    continueToNextResponse = false;
+
+                    result.body.on('readable', function () {
+                        let data;
+                        while (data = this.read()) {
+                            res.write(data);
+                        }
+                    });
+
+                    result.body.on('finish', function (err) {
+                        res.end();
+                        next();
+                    });
+
+                    result.body.on('error', function () {
+                        res.end();
+                        next();
+                    });
+                    break;
+                default:
+                    if (result.body)
+                        res.write(result.body);
+                    break;
+            }
+
+        }
+
+        if (continueToNextResponse) {
+            res.end();
+            next();
+        }
+    };
+
+    function startServer(portNumber) {
         let server = restify.createServer();
 
         for (let mKey in routes)
-            for (let mParam in routes[mKey]) {
-                let inObject = {
-                    lambda: routes[mKey][mParam],
-                    method: mKey
-                };
+        for (let mParam in routes[mKey]) {
+            let inObject = {
+                lambda: routes[mKey][mParam],
+                method: mKey
+            };
 
-                let eUnit = new ExecutableUnit(inObject, filterManager);
-                let newPath;
-                if (mParam.includes("{")) {
-                    let splitData = mParam.split("/");
-                    newPath = "";
-                    for (let j = 0; j < splitData.length; j++) {
-                        let fItem = splitData[j];
-                        if (fItem.includes("{"))
-                            fItem = ":" + (fItem.replace("{", "").replace("}", ""));
-                        newPath += ("/" + fItem);
-                    }
-                } else newPath = mParam;
+            let rHandler = new SteroidsServiceHandler(inObject, filterManager);
+            let newPath;
+            if (mParam.includes("{")) {
+                let splitData = mParam.split("/");
+                newPath = "";
+                for (let j = 0; j < splitData.length; j++) {
+                    let fItem = splitData[j];
+                    if (fItem.includes("{"))
+                        fItem = ":" + (fItem.replace("{", "").replace("}", ""));
+                    newPath += ("/" + fItem);
+                }
+            } else newPath = mParam;
 
-                server[mKey](newPath, eUnit.handle);
-            }
+            server[mKey](newPath, (req,res,next)=>{
+                rHandler.handle(req,res,next, (result,context)=>{
+                    processResponse(req,res,next,result,context);
+                });
+            });
+        }
+
 
         server.use(restify.acceptParser(server.acceptable));
         server.use(restify.jsonp());
@@ -339,9 +337,6 @@ function MsfCore() {
     }
 
     function loadServerless() {
-        let yaml = require('js-yaml');
-        let fs = require('fs');
-
         try {
             let ymlData = yaml.safeLoad(fs.readFileSync('serverless.yml', 'utf8'));
 
@@ -405,7 +400,7 @@ function MsfCore() {
             filterManager.register(filterFunc);
         },
         loadConfiguration: loadConfiguration,
-        start: startRoutingEngine
+        start: startServer
     }
 }
 
@@ -413,4 +408,4 @@ process.on('uncaughtException', function (err) {
     console.log(err);
 })
 
-module.exports = new MsfCore();
+module.exports = new SteroidsRuntime();
